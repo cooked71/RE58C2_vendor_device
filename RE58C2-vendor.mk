76e58cbf97c63d740a8bdfe36771d0b5129e2bab
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-08-03 18:07:44 +0200

    Update RE58C2-vendor.mk

--- RE58C2-vendor.mk
+++ RE58C2-vendor.mk
@@ -489,8 +489,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*.ko,vendor/realme/RE58C2/proprietary/vendor_dlkm/lib/modules,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules)
 
-PRODUCT_COPY_FILES += \
-     vendor/realme/RE58C2/proprietary/vendor_dlkm/etc/init/init.realme.rc:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init/init.realme.rc
+#PRODUCT_COPY_FILES += \
+#     vendor/realme/RE58C2/proprietary/vendor_dlkm/etc/init/init.realme.rc:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init/init.realme.rc
 
 # Module metadata files
 PRODUCT_COPY_FILES += \
