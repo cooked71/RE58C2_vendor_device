670185e20e1c8af6fc6b279d8c0483ef39fbb2fa
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-08-25 12:27:57 +0200

    save

--- RE58C2-vendor.mk
+++ RE58C2-vendor.mk
@@ -636,7 +636,7 @@ PRODUCT_PACKAGES += \
 # ========================
 # Modem/Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors-service.default \
     rild
 
 # ========================
